dff133b8401ce3aeafe8779caa75fba07c3726b4
wwdfdo <wwdfdo@gmail.com>
2023-03-27 15:16:39 +0530

    added new styles to scroll bar

--- src/components/LatestDetails/LatestDetails.js
+++ src/components/LatestDetails/LatestDetails.js
@@ -1,95 +1,200 @@
 import React from "react";
+import "./LatestDetails.css";
 
 const LatestDetails = () => {
   return (
     <div className="w-3/4 mx-auto gap-10 flex justify-around  py-10 ">
-      <div className=" flex flex-col gap-2 bg-slate-100 w-1/2  py-5 rounded-xl px-5 ">
-        <h4 className="px-5  py-3 text-lg font-bold">Latest Blocks</h4>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
-            BK
+      <div className="w-[100%] bg-slate-100 rounded-xl pt-5  px-2">
+        <div className=" flex flex-col gap-2    h-[500px] overflow-hidden hover:overflow-auto    px-5 ">
+          <h4 className="px-5  py-3 text-lg font-bold">Latest Blocks</h4>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
           </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
+              BK
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
         </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
-            BK
-          </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
-          </div>
+        <div className="flex justify-center items-center w-full  rounded-br-lg rounded-bl-lg py-4 border-t-2 border-black border-opacity-25">
+          <button className="text-white bg-blue-500 w-3/4 rounded-2xl py-1 text-sm">
+            View more
+          </button>
         </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
-            BK
-          </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
-          </div>
-        </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-xl">
-            BK
-          </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
-          </div>
-        </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
       </div>
-      <div className=" flex flex-col gap-2 bg-slate-100 py-5 w-1/2 rounded-xl px-5 ">
-        <h4 className="px-5 py-3 text-lg font-bold">Latest Blocks</h4>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
-            Tx
+      <div className="w-[100%] bg-slate-100 rounded-xl pt-5 px-2">
+        <div className=" flex flex-col gap-2 bg-slate-100   h-[500px] overflow-hidden hover:overflow-auto  px-5 ">
+          <h4 className="px-5 py-3 text-lg font-bold">Latest Blocks</h4>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
           </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs0 ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
+          <div className="flex gap-4 items-center px-3">
+            <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
+              Tx
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>72776138</div>
+              <div>33 secs ago4 txns in 0 secs ETH</div>
+            </div>
+          </div>
+          <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
         </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
-            Tx
-          </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
-          </div>
+        <div className="flex justify-center w-full  rounded-br-lg rounded-bl-lg py-4 border-t-2 border-black border-opacity-25">
+          <button className="text-white bg-blue-500 w-3/4 rounded-2xl py-1 text-sm">
+            View more
+          </button>
         </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
-            Tx
-          </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs0 ETH</div>
-          </div>
-        </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
-        <div className="flex gap-4 items-center px-3">
-          <div className="w-8 h-8 bg-red-300 flex justify-center items-center rounded-full">
-            Tx
-          </div>
-          <div className="flex flex-col gap-1">
-            <div>72776138</div>
-            <div>33 secs ago4 txns in 0 secs ETH</div>
-          </div>
-        </div>
-        <hr className="border-[.5px] border-black border-opacity-40 mx-3"></hr>
       </div>
     </div>
   );
